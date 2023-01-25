7a4086f29a29b46d58ac1a6034ab8f17ff1ed58a
mike <michaelmussa9@gmail.com>
2023-01-25 18:15:24 +0300

    jwt token added

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,27 @@
+const User = require('../models/User');
+const { verifyTokenandAuth } = require('./verifyToken');
+
+
 const router = require('express').Router();
+
+router.put('/:id',verifyTokenandAuth,async (req,res)=>{
+  if (req.body.password) {
+    req.body.password=crytojs.AES.encrypt(req.body.password,process.env.PASS_SEC).toString();
+    
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+        $set:req.body
+    },{new:true});
+    res.status(200).json(updatedUser);
+    
+  } catch (error) {
+    res.status(500).json(error);
+
+    
+  }
+});
 
 
 module.exports = router;
